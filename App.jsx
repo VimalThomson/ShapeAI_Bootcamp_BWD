1ab0ac5b1447ae640718917791e3f61d885f1412
VimalThomson <77142203+VimalThomson@users.noreply.github.com>
2021-06-26 22:03:40 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Footer from "./Footer.jsx";
+import Header from "./Header.jsx";
+import Note from "./Note.jsx";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <h1> </h1>
+
+      <Footer />
+      <Note />
+      <Note />
+      <Note />
+      <Note />
+      <Note />
+      <Note />
+      <Note />
+      <Note />
+      <Note />
+      <Note />
+    </div>
+  );
+}
+export default App;
---
+++ Note.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div class="note">
+      <h1> This is a card.</h1>
+      <h6> This can be duplicated.</h6>
+    </div>
+  );
+}
+export default Note;
